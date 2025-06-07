c83a3e82fe8b54878f36ab81e937fbfc0fab4dd9
tim <ctom3560@gmail.com>
2025-06-08 03:43:02 +0800

    update

---
+++ concurrency-mapreduce/mapreduce.c
@@ -0,0 +1,22 @@
+#include <pthread.h>
+
+#include "mapreduce.h"
+
+
+void MR_Emit(char *key, char *value) {
+
+}
+
+unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *key++) != '\0')
+        hash = hash * 33 + c;
+    return hash % num_partitions;
+}
+
+
+void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce, int num_reducers, Partitioner partition) {
+
+
+}
