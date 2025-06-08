a15a6626d7fae8ced61406986a7030bd8c4b9f21
Kam Wing Yiu <ctom3560@gmail.com>
2025-06-08 16:53:07 +0800

    update

--- concurrency-mapreduce/mapreduce.c
+++ concurrency-mapreduce/mapreduce.c
@@ -1,6 +1,32 @@
 #include <pthread.h>
+#include <stdlib.h>
 
 #include "mapreduce.h"
+
+typedef struct KVs {
+    char *key;
+    char **value;
+    int size; // number of values for this key
+    int capacity; // used to grow the value array
+    int index; // used in reducer to track which value to return next; each KVs only reduces once
+} KVs;
+
+typedef struct KVsStore {
+    KVs *kvs;
+    pthread_semaphore key_sem;
+} KVsStore;
+
+void KVsStore_Init(KeyValuesStore *store) {
+}
+
+void KVsStore_Insert(KeyValuesStore *store, char *key, char *value) {
+}
+
+void KVsStore_Lookup(KeyValuesStore *store) {
+}
+
+void KVsStore_Free(KeyValuesStore *store) {
+}
 
 
 void MR_Emit(char *key, char *value) {
