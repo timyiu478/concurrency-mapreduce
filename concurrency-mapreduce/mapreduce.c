007e6b37fe9ed1a116ec9f077f278b0e2d5ab553
tim <ctom3560@gmail.com>
2025-06-09 00:50:33 +0800

    update

--- concurrency-mapreduce/mapreduce.c
+++ concurrency-mapreduce/mapreduce.c
@@ -2,17 +2,20 @@
 #include <semaphore.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "mapreduce.h"
 
 #define DYNAMIC_ARRAY_CAPACITY 64
+#define DEBUG 1
 
 typedef struct KVs {
     char *key;
-    char **value;
+    char **values;
     int size; // number of values for this key
     int capacity; // used to grow the value array
     int index; // used in reducer to track which value to return next; each KVs only reduces once
+    sem_t sem; // semaphore to control access to the values or index
 } KVs;
 
 typedef struct KVsStore {
@@ -24,7 +27,7 @@ typedef struct KVsStore {
 } KVsStore;
 
 typedef struct MapperTasks {
-    char **file_name; // The file names to be processed by the mapper
+    char **file_names; // The file names to be processed by the mapper
     int num_files; // Number of files to be processed by the mapper
     int index; // current index of the file being processed
     sem_t sem; // Semaphore to control access to the file names
@@ -38,7 +41,7 @@ Reducer global_reduce;
 Partitioner global_partition;
 
 void MapperTasks_Init(MapperTasks *tasks, char **file_names, int num_files) {
-    tasks->file_name = file_names;
+    tasks->file_names = file_names;
     tasks->num_files = num_files;
     tasks->index = 0;
     sem_init(&tasks->sem, 0, 1);
@@ -46,16 +49,19 @@ void MapperTasks_Init(MapperTasks *tasks, char **file_names, int num_files) {
 
 void MapperTasks_Free(MapperTasks *tasks) {
     for (int i = 0; i < tasks->num_files; i++) {
-        free(tasks->file_name[i]);
+        free(tasks->file_names[i]);
     }
-    free(tasks->file_name);
+    free(tasks->file_names);
     sem_destroy(&tasks->sem);
 }
 
 char *get_mapper_task(MapperTasks *tasks) {
     sem_wait(&tasks->sem);
     if (tasks->index < tasks->num_files) {
-        return tasks->file_name[tasks->index++];
+        int tmp = tasks->index;
+        tasks->index++;
+        sem_post(&tasks->sem);
+        return tasks->file_names[tmp];
     }
     sem_post(&tasks->sem);
     return NULL; // No more files to process
@@ -64,6 +70,9 @@ char *get_mapper_task(MapperTasks *tasks) {
 void *mapper_worker(MapperTasks *tasks) {
     char *file_name = get_mapper_task(tasks);
     while (file_name != NULL) {
+        if (DEBUG) {
+            printf("Mapper processing file: %s\n", file_name);
+        }
         global_map(file_name);
         file_name = get_mapper_task(tasks);
     }
@@ -72,10 +81,11 @@ void *mapper_worker(MapperTasks *tasks) {
 
 void KVs_Init(KVs *kvs) {
     kvs->key = NULL;
-    kvs->value = malloc(DYNAMIC_ARRAY_CAPACITY * sizeof(char *));
+    kvs->values = malloc(DYNAMIC_ARRAY_CAPACITY * sizeof(char *));
     kvs->size = 0;
     kvs->capacity = DYNAMIC_ARRAY_CAPACITY;
     kvs->index = 0;
+    sem_init(&kvs->sem, 0, 1);
 }
 
 void KVs_Free(KVs *kvs) {
@@ -83,14 +93,30 @@ void KVs_Free(KVs *kvs) {
         free(kvs->key);
     }
     for (int i = 0; i < kvs->size; i++) {
-        free(kvs->value[i]);
+        free(kvs->values[i]);
     }
-    free(kvs->value);
+    free(kvs->values);
+    sem_destroy(&kvs->sem);
 }
 
 void KVs_Resize(KVs *kvs) {
     kvs->capacity *= 2;
-    kvs->value = realloc(kvs->value, kvs->capacity * sizeof(char *));
+    kvs->values = realloc(kvs->values, kvs->capacity * sizeof(char *));
+}
+
+// Function to get the next value for a key in KVs
+char *KVs_GetValue(KVs *kvs) {
+    if (DEBUG) {
+        printf("KVs_GetValue called for key: %s, current index: %d, size: %d\n", kvs->key, kvs->index, kvs->size);
+    }
+    if (kvs->index >= kvs->size) {
+        return NULL; // Index out of bounds
+    }
+    sem_wait(&kvs->sem);
+    int index = kvs->index;
+    kvs->index++;
+    sem_post(&kvs->sem);
+    return kvs->values[index];
 }
 
 void KVsStore_Init(KVsStore *store) {
@@ -121,9 +147,12 @@ void KVsStore_Insert(KVsStore *store, char *key, char *value) {
             if (store->kvs[i].size >= store->kvs[i].capacity) {
                 KVs_Resize(&store->kvs[i]);
             }
-            store->kvs[i].value[store->kvs[i].size] = strdup(value);
+            store->kvs[i].values[store->kvs[i].size] = strdup(value);
             store->kvs[i].size++;
             sem_post(&store->sem);
+            if (DEBUG) {
+                printf("KVsStore_Insert: Key %s already exists, added value %s\n", key, value);
+            }
             return;
         }
     }
@@ -134,16 +163,22 @@ void KVsStore_Insert(KVsStore *store, char *key, char *value) {
 
     // If the key does not exist, add a new KVs entry
     store->kvs[store->size].key = strdup(key);
-    store->kvs[store->size].value[0] = strdup(value);
+    store->kvs[store->size].values[0] = strdup(value);
     store->kvs[store->size].size = 1;
     store->size++;
-
     sem_post(&store->sem);
+
+    if (DEBUG) {
+        printf("KVsStore_Insert: Inserted key %s with value %s\n", key, value);
+    }
 }
 
 KVs* KVsStore_Lookup(KVsStore *store, char *key) {
     sem_wait(&store->sem);
     for (int i = 0; i < store->size; i++) {
+        if (DEBUG) {
+            printf("KVsStore_Lookup checking key: %s against %s\n", store->kvs[i].key, key);
+        }
         if (store->kvs[i].key && strcmp(store->kvs[i].key, key) == 0) {
             sem_post(&store->sem);
             return &store->kvs[i];
@@ -162,17 +197,20 @@ void KVsStore_Free(KVsStore *store) {
 }
 
 char *Reducer_Getter(char *key, int partition_number) {
+    if (DEBUG) {
+        printf("Reducer_Getter called for key: %s in partition: %d\n", key, partition_number);
+    }
     KVsStore *kvs_store = &partition_stores[partition_number];
     KVs *kvs = KVsStore_Lookup(kvs_store, key);
-    if (kvs != NULL && kvs->index < kvs->size) {
-        return kvs->value[kvs->index++];
-    }
-    return NULL; // No more values
+    return KVs_GetValue(kvs);
 }
 
 void MR_Emit(char *key, char *value) {
-    unsigned long partition_number = global_partition(key, global_num_partitions);
+    int partition_number = global_partition(key, global_num_partitions);
     KVsStore_Insert(&partition_stores[partition_number], key, value);
+    if (DEBUG) {
+        printf("Emitted key: %s, value: %s to partition: %d\n", key, value, partition_number);
+    }
 }
 
 unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
@@ -185,15 +223,16 @@ unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
 
 void *reduce_worker(int *partition_number) {
     KVsStore *store = &partition_stores[*partition_number];
-    sem_wait(&store->sem);
     for (int i = 0; i < store->size; i++) {
         KVs *kvs = &store->kvs[i];
         if (kvs->size > 0) {
             // Call the reducer function for each key
+            if (DEBUG) {
+                printf("Reducer processing key: %s in partition: %d\n", kvs->key, *partition_number);
+            }
             global_reduce(kvs->key, Reducer_Getter, *partition_number);
         }
     }
-    sem_wait(&store->sem);
     return NULL;
 }
 
@@ -224,10 +263,9 @@ void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce,
     MapperTasks tasks;
     char **file_names = malloc((argc - 1) * sizeof(char *));
     for (int i = 1; i < argc; i++) {
-        file_names[i - 1] = argv[i];
+        file_names[i - 1] = strdup(argv[i]);
     }
     MapperTasks_Init(&tasks, file_names, argc - 1);
-
 
     // Run num_mappers number of mappers in parallel
     pthread_t *mapper_threads = malloc(num_mappers * sizeof(pthread_t));
