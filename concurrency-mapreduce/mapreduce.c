322df17baeeddacd4cab17840516f2d376280d6c
tim <ctom3560@gmail.com>
2025-06-08 20:41:37 +0800

    update

--- concurrency-mapreduce/mapreduce.c
+++ concurrency-mapreduce/mapreduce.c
@@ -1,7 +1,11 @@
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "mapreduce.h"
+
+#define DYNAMIC_ARRAY_CAPACITY 64
 
 typedef struct KVs {
     char *key;
@@ -13,24 +17,162 @@ typedef struct KVs {
 
 typedef struct KVsStore {
     KVs *kvs;
-    pthread_semaphore key_sem;
+    int size;
+    int capacity;
+    int index; // used to track the current KVs being processed
+    sem_t sem;
 } KVsStore;
 
-void KVsStore_Init(KeyValuesStore *store) {
+typedef struct MapperTasks {
+    char **file_name; // The file names to be processed by the mapper
+    int num_files; // Number of files to be processed by the mapper
+    int index; // current index of the file being processed
+    sem_t sem; // Semaphore to control access to the file names
+} MapperTasks;
+
+KVsStore *partition_stores; // Global variable to hold partition stores so that they can be accessed by the Mapper and Reducer functions.
+
+int global_num_partitions;
+Mapper global_map;
+Reducer global_reduce;
+Partitioner global_partition;
+
+void MapperTasks_Init(MapperTasks *tasks, char **file_names, int num_files) {
+    tasks->file_name = file_names;
+    tasks->num_files = num_files;
+    tasks->index = 0;
+    sem_init(&tasks->sem, 0, 1);
 }
 
-void KVsStore_Insert(KeyValuesStore *store, char *key, char *value) {
+void MapperTasks_Free(MapperTasks *tasks) {
+    for (int i = 0; i < tasks->num_files; i++) {
+        free(tasks->file_name[i]);
+    }
+    free(tasks->file_name);
+    sem_destroy(&tasks->sem);
 }
 
-void KVsStore_Lookup(KeyValuesStore *store) {
+char *get_mapper_task(MapperTasks *tasks) {
+    sem_wait(&tasks->sem);
+    if (tasks->index < tasks->num_files) {
+        return tasks->file_name[tasks->index++];
+    }
+    sem_post(&tasks->sem);
+    return NULL; // No more files to process
 }
 
-void KVsStore_Free(KeyValuesStore *store) {
+void *mapper_worker(MapperTasks *tasks) {
+    char *file_name = get_mapper_task(tasks);
+    while (file_name != NULL) {
+        global_map(file_name);
+        file_name = get_mapper_task(tasks);
+    }
+    return NULL;
 }
 
+void KVs_Init(KVs *kvs) {
+    kvs->key = NULL;
+    kvs->value = malloc(DYNAMIC_ARRAY_CAPACITY * sizeof(char *));
+    kvs->size = 0;
+    kvs->capacity = DYNAMIC_ARRAY_CAPACITY;
+    kvs->index = 0;
+}
+
+void KVs_Free(KVs *kvs) {
+    if (kvs->key) {
+        free(kvs->key);
+    }
+    for (int i = 0; i < kvs->size; i++) {
+        free(kvs->value[i]);
+    }
+    free(kvs->value);
+}
+
+void KVs_Resize(KVs *kvs) {
+    kvs->capacity *= 2;
+    kvs->value = realloc(kvs->value, kvs->capacity * sizeof(char *));
+}
+
+void KVsStore_Init(KVsStore *store) {
+    store->kvs = malloc(DYNAMIC_ARRAY_CAPACITY * sizeof(KVs));
+    store->size = 0;
+    store->index = 0;
+    store->capacity = DYNAMIC_ARRAY_CAPACITY;
+    for (int i = 0; i < DYNAMIC_ARRAY_CAPACITY; i++) {
+        KVs_Init(&store->kvs[i]);
+    }
+    sem_init(&store->sem, 0, 1);
+}
+
+void KVsStore_Resize(KVsStore *store) {
+    store->capacity *= 2;
+    store->kvs = realloc(store->kvs, store->capacity * sizeof(KVs));
+    for (int i = store->size; i < store->capacity; i++) {
+        KVs_Init(&store->kvs[i]);
+    }
+}
+
+void KVsStore_Insert(KVsStore *store, char *key, char *value) {
+    sem_wait(&store->sem);
+
+    // Check if the key already exists
+    for (int i = 0; i < store->size; i++) {
+        if (store->kvs[i].key && strcmp(store->kvs[i].key, key) == 0) {
+            if (store->kvs[i].size >= store->kvs[i].capacity) {
+                KVs_Resize(&store->kvs[i]);
+            }
+            store->kvs[i].value[store->kvs[i].size] = strdup(value);
+            store->kvs[i].size++;
+            sem_post(&store->sem);
+            return;
+        }
+    }
+
+    if (store->size >= store->capacity) {
+        KVsStore_Resize(store);
+    }
+
+    // If the key does not exist, add a new KVs entry
+    store->kvs[store->size].key = strdup(key);
+    store->kvs[store->size].value[0] = strdup(value);
+    store->kvs[store->size].size = 1;
+    store->size++;
+
+    sem_post(&store->sem);
+}
+
+KVs* KVsStore_Lookup(KVsStore *store, char *key) {
+    sem_wait(&store->sem);
+    for (int i = 0; i < store->size; i++) {
+        if (store->kvs[i].key && strcmp(store->kvs[i].key, key) == 0) {
+            sem_post(&store->sem);
+            return &store->kvs[i];
+        }
+    }
+    sem_post(&store->sem);
+    return NULL; // Key not found
+}
+
+void KVsStore_Free(KVsStore *store) {
+    for (int i = 0; i < store->size; i++) {
+        KVs_Free(&store->kvs[i]);
+    }
+    free(store->kvs);
+    sem_destroy(&store->sem);
+}
+
+char *Reducer_Getter(char *key, int partition_number) {
+    KVsStore *kvs_store = &partition_stores[partition_number];
+    KVs *kvs = KVsStore_Lookup(kvs_store, key);
+    if (kvs != NULL && kvs->index < kvs->size) {
+        return kvs->value[kvs->index++];
+    }
+    return NULL; // No more values
+}
 
 void MR_Emit(char *key, char *value) {
-
+    unsigned long partition_number = global_partition(key, global_num_partitions);
+    KVsStore_Insert(&partition_stores[partition_number], key, value);
 }
 
 unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
@@ -41,8 +183,89 @@ unsigned long MR_DefaultHashPartition(char *key, int num_partitions) {
     return hash % num_partitions;
 }
 
+void *reduce_worker(int *partition_number) {
+    KVsStore *store = &partition_stores[*partition_number];
+    sem_wait(&store->sem);
+    for (int i = 0; i < store->size; i++) {
+        KVs *kvs = &store->kvs[i];
+        if (kvs->size > 0) {
+            // Call the reducer function for each key
+            global_reduce(kvs->key, Reducer_Getter, *partition_number);
+        }
+    }
+    sem_wait(&store->sem);
+    return NULL;
+}
+
+// Comparison function for qsort
+int compareKVs(const void *a, const void *b) {
+    const KVs *kv1 = (const KVs *)a;
+    const KVs *kv2 = (const KVs *)b;
+
+    return strcmp(kv1->key, kv2->key);
+}
+
 
 void MR_Run(int argc, char *argv[], Mapper map, int num_mappers, Reducer reduce, int num_reducers, Partitioner partition) {
 
+    // Set the global variables
+    global_map = map;
+    global_reduce = reduce;
+    global_partition = partition;
+    global_num_partitions = num_reducers;
+
+    // Initialize partition stores
+    partition_stores = malloc(global_num_partitions * sizeof(KVsStore));
+    for (int i = 0; i < global_num_partitions; i++) {
+        KVsStore_Init(&partition_stores[i]);
+    }
+
+    // Initialize mapper tasks
+    MapperTasks tasks;
+    char **file_names = malloc((argc - 1) * sizeof(char *));
+    for (int i = 1; i < argc; i++) {
+        file_names[i - 1] = argv[i];
+    }
+    MapperTasks_Init(&tasks, file_names, argc - 1);
+
+
+    // Run num_mappers number of mappers in parallel
+    pthread_t *mapper_threads = malloc(num_mappers * sizeof(pthread_t));
+    for (int i = 0; i < num_mappers; i++) {
+        pthread_create(&mapper_threads[i], NULL, (void *)mapper_worker, &tasks);
+    }
+
+    // Wait for all mappers to finish
+    for (int i = 0; i < num_mappers; i++) {
+        pthread_join(mapper_threads[i], NULL);
+    }
+
+    // Free mapper tasks & threads
+    MapperTasks_Free(&tasks);
+    free(mapper_threads);
+
+    // Sorting
+    for (int i = 0; i < global_num_partitions; i++) {
+        KVsStore *store = &partition_stores[i];
+        // Sort the keys in the store
+        qsort(store->kvs, store->size, sizeof(KVs), compareKVs);
+    }
+
+    // Run reducers
+    pthread_t *reducer_threads = malloc(num_mappers * sizeof(pthread_t));
+    for (int i = 0; i < num_reducers; i++) {
+        pthread_create(&reducer_threads[i], NULL, (void *)reduce_worker, &i);
+    }
+
+    // Wait for all reducers to finish
+    for (int i = 0; i < num_reducers; i++) {
+        pthread_join(reducer_threads[i], NULL);
+    }
+
+    // Free reducer threads & partition stores
+    free(reducer_threads);
+    for (int i = 0; i < global_num_partitions; i++) {
+        KVsStore_Free(&partition_stores[i]);
+    }
 
 }
